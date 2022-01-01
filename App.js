5871f3b533c0fa9a218e49fe7b9baa18739a8d10
SangHoon <joa3668@gmail.com>
2022-01-01 16:14:33 +0900

    Add: font,image preload

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, Image } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
+import { Asset, useAssets } from 'expo-asset';
+
+// const loadFonts = fonts => fonts.map(font => Font.loadAsync(font));
+
+// const loadImages = images =>
+// 	images.map(image => {
+// 		if (typeof image === 'string') {
+// 			return Image.prefetch(image);
+// 		} else {
+// 			return Asset.loadAsync(image);
+// 		}
+// 	});
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+	// const [ready, setReady] = useState(false);
+	// const onFinish = () => setReady(true);
+	// const startLoading = async () => {
+	// 	const fonts = loadFonts([Ionicons.font]);
+	// 	const images = loadImages([require('./Ho0on.png'), 'https://d33wubrfki0l68.cloudfront.net/b152eb4214943f96e83c4babde026b12221e68f1/a20c2/img/oss_logo.png']);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	// 	await Promise.all([...fonts, ...images]);
+	// };
+
+	const [assets] = useAssets([require('./Ho0on.png')]);
+	const [loaded] = Font.useFonts(Ionicons.font);
+
+	if (!assets || !loaded) {
+		return <AppLoading />;
+	}
+	return <Text>sadfasdfsdf</Text>;
+}
